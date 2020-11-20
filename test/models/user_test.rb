6348ccb79d7408b37ea702ed116b9b49c6c934d6
JoTakahashi <jo.takahashi.0120@gmail.com>
2020-11-20 16:20:08 +0000

    [Update]Userモデルにバリデーションを設定

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,49 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @user = User.new(name: "Example User", email: "user@example.com")
+  end
+  
+  test "should be valid" do
+    assert @user.valid?
+  end
+  
+  test "name should be present" do
+    @user.name = "   "
+    assert_not @user.valid?
+  end
+  
+  test "email should be present" do
+    @user.email = "   "
+    assert_not @user.valid?
+  end
+  
+  test "name should not be too long" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+  
+  test "email should not be too long" do
+    @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid?
+  end
+  
+  test "email validation should accept valid addresses" do
+    valid_addresses = %w[user@example.com USER@foo.COM  A_US-ER@foo.bar.org
+                         first.late@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_addresse|
+      @user.email = valid_addresse
+      assert @user.valid?, "#{valid_addresse.inspect} should be valid"
+    end
+  end
+  
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_addresse|
+      @user.email = invalid_addresse
+      assert_not @user.valid?, "#{invalid_addresse.inspect} should be invalid"
+    end
+  end
 end
