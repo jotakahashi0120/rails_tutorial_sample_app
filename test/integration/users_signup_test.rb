008d5934739e74680bd4019ef23468ac1e9adc1e
JoTakahashi <jo.takahashi.0120@gmail.com>
2020-11-22 15:23:15 +0000

    [Update]users_signupの統合テストにテストを追加

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -13,4 +13,16 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password_confirmation: "bar" } }
     end
   end
+  
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count' do
+      post users_path, params: { user: { name:  "Example User",
+                                         email: "user@example.com",
+                                         password:              "password",
+                                         password_confirmation: "password" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+  end
 end
